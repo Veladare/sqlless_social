98da68f3ca8c2f2e71c0bb5b7ab0c67677b7be68
Jean-Luc <jeanlucv@berkeley.edu>
2023-09-07 12:27:14 -0700

    added delete route for User

--- controller/userController.js
+++ controller/userController.js
@@ -17,7 +17,20 @@ const userController = {
         console.error(err);
         res.sendStatus(400);
       }
-      
+    },
+    async getSingleUser(req, res) {
+      try {
+        const user = await User.findOne({ _id: req.params.userId })
+          .select('-__v');
+  
+        if (!user) {
+          return res.status(404).json({ message: 'No user with that ID' });
+        }
+  
+        res.json(user);
+      } catch (err) {
+        res.status(500).json(err);
+      }
     },
     async createUser(req, res) {
       try {
@@ -27,7 +40,23 @@ const userController = {
         res.status(500).json(err);
       }
     },
-  };
+    async deleteSingleUser(req, res) {
+      try {
+        const deletedUser = await User.findOneAndDelete({ _id: req.params.userId });
   
+        if (!deletedUser) {
+          return res.status(404).json({ message: 'No user with that ID' });
+        }
+  
+        // Optionally, you can also delete references to this user from other collections,
+        // such as removing the user from the 'friends' list of other users.
+  
+        res.json({ message: 'User deleted successfully', deletedUser });
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    },
+    
+};
 
   module.exports = userController
--- routes/api-routes/userRoutes.js
+++ routes/api-routes/userRoutes.js
@@ -2,9 +2,13 @@ const router = require("express").Router();
 const {
     getAllUser,
     createUser,
+    getSingleUser,
+    deleteSingleUser
 
 } = require('../../controller/userController')
 
 router.route("/").get(getAllUser).post(createUser)
+
+router.route('/:userId').get(getSingleUser).delete(deleteSingleUser)
 
 module.exports = router;
